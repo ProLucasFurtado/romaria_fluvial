500ede5958812656207d9fe27e1750c3c6c4b41b
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2022-07-03 05:10:03 -0300

    correção de erro de compra de ingresso 1

--- src/components/PurchaseButton/index.tsx
+++ src/components/PurchaseButton/index.tsx
@@ -11,6 +11,11 @@ export function PurchaseButton({ priceId }: PurchaseButtonProps) {
   const session = useSession();
 
   async function handlePurchase() {
+    if (!session) {
+      signIn('cognito');
+      return;
+    }
+
     try {
       const response = await api.post('purchase');
 
@@ -21,18 +26,19 @@ export function PurchaseButton({ priceId }: PurchaseButtonProps) {
       await stripe?.redirectToCheckout({ sessionId });
     } catch (error) {
       alert('Houve um erro!');
+      console.log('error', error);
     }
   }
 
   return (
     <Button
       colorScheme="green"
-      size="lg"
       _disabled={{
         bg: 'blue.500',
         cursor: 'default',
       }}
       onClick={handlePurchase}
+      size="lg"
     >
       C. Crédito ou Boleto
     </Button>
